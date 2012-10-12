14b1188e9de5d356833f33449fb9bf7d090af7e1
Mélanie Bats <melanie.bats@obeo.fr>
2012-10-12 10:57:24 +0200

    Test and cast element to DataPort in getDirectedPorts service

--- plugins/org.obeonetwork.dsl.aadl.design/src/org/obeonetwork/dsl/aadl/design/AadlServices.java
+++ plugins/org.obeonetwork.dsl.aadl.design/src/org/obeonetwork/dsl/aadl/design/AadlServices.java
@@ -1,32 +1,26 @@
 package org.obeonetwork.dsl.aadl.design;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.osate.aadl2.Aadl2Package;
 import org.osate.aadl2.AadlPackage;
 import org.osate.aadl2.ComponentType;
 import org.osate.aadl2.DataPort;
 import org.osate.aadl2.DirectionType;
-import org.osate.aadl2.SystemType;
-import org.osate.aadl2.ThreadType;
 
 public class AadlServices {
 
 	public ArrayList<AadlPackage> getPackages(EObject model) {
 
 		/*
-		 * public List<AadlPackage> getPackages(EObject model){
-		 * Get all the resources of the resource set 
-		 * // List<Resource> aadlFiles = resSet.getResources(); 
-		 * // Clean the aadlFiles to get only the aadl files 
-		 * // For each resource get the aadl package element 
-		 * // Add it to a result list 
-		 * // Return the aadl packages
+		 * public List<AadlPackage> getPackages(EObject model){ Get all the
+		 * resources of the resource set // List<Resource> aadlFiles =
+		 * resSet.getResources(); // Clean the aadlFiles to get only the aadl
+		 * files // For each resource get the aadl package element // Add it to
+		 * a result list // Return the aadl packages
 		 */
 		System.out
 				.println("\n============================= Starting AADLServices ===========================");
@@ -48,12 +42,15 @@ public class AadlServices {
 				TreeIterator<EObject> i = r.getAllContents();
 				while (i.hasNext()) {
 					EObject checkingResourceClassName = i.next();
-					//checking if class is AadlPackage
-					if (checkingResourceClassName.eClass().getName().equals("AadlPackage")) {
-						//printing the name
-						System.out.println(checkingResourceClassName.eClass().getName());
-						aadlPackageList.add((AadlPackage)checkingResourceClassName);
-						
+					// checking if class is AadlPackage
+					if (checkingResourceClassName.eClass().getName()
+							.equals("AadlPackage")) {
+						// printing the name
+						System.out.println(checkingResourceClassName.eClass()
+								.getName());
+						aadlPackageList
+								.add((AadlPackage) checkingResourceClassName);
+
 					}
 				}
 
@@ -67,9 +64,9 @@ public class AadlServices {
 				.println("============================= Ending AADLServices ===========================\n");
 		return aadlPackageList;
 	}
-	
+
 	public ArrayList<ComponentType> getComponents(EObject model, String componentType){
-		ArrayList<ComponentType> componentsCollectionResult = new ArrayList<>();
+		ArrayList<ComponentType> componentsCollectionResult = new ArrayList<ComponentType>();
 	
 
 		//getting the iterator for all the objects
@@ -89,25 +86,34 @@ public class AadlServices {
 		
 		return componentsCollectionResult;
 	}
-	
-	public void getDirectedPorts(EObject model, String portDirection){
-		System.out.println("<<<<<<<<<<<<<<<<<<<==========getDirectedPorts==============>>>>>>>>>>>>>>>>>>>>>\n");
-		//System.out.println(model.eContents());
+
+	public void getDirectedPorts(EObject model, String portDirection) {
+		System.out
+				.println("<<<<<<<<<<<<<<<<<<<==========getDirectedPorts==============>>>>>>>>>>>>>>>>>>>>>\n");
+		// System.out.println(model.eContents());
 		TreeIterator<EObject> i = model.eAllContents();
 		DirectionType dt;
-		while (i.hasNext()){
+		while (i.hasNext()) {
 			EObject singleResource = i.next();
-			
-			//finding the desired component Type
-			if (singleResource.eClass().getName().equals("DataPort")){
-		
-				//System.out.println("Attributes: " + singleResource.eClass().getEAllAttributes().get(2));
-				System.out.println(singleResource);
+
+			// finding the desired component Type
+			if (singleResource instanceof DataPort) {
+				DataPort port = (DataPort) singleResource;
+				// System.out.println("Attributes: " +
+				// singleResource.eClass().getEAllAttributes().get(2));
+				DirectionType direction = port.getDirection();
+				System.out.println(port.getDirection());
+				if (direction.incoming())
+					System.out.println("Port : " + port.getName()
+							+ " is an input port");
+				if (direction.outgoing())
+					System.out.println("Port : " + port.getName()
+							+ " is an output port");
 			}
 		}
-		
-		System.out.println("\n<<<<<<<<<<<<<<<<<<<=========getDirectedPorts===============>>>>>>>>>>>>>>>>>>>>>\n");
+
+		System.out
+				.println("\n<<<<<<<<<<<<<<<<<<<=========getDirectedPorts===============>>>>>>>>>>>>>>>>>>>>>\n");
 	}
-	
-	
+
 }
